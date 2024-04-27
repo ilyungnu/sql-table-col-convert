d92121f68ba88e7128e83ec6bce17c9f42f834a1
neromalion <neromalion@gmail.com>
2024-04-27 11:31:33 +0900

    refact: deduplication, rename parameter

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const tableCol = document.querySelector('#tableCol');
 const sqlBefore = document.getElementById('sqlBefore');
 
 //toggle all
-const ToggleAll = (table) => {
-  let headChkbox = table.querySelector('thead input[type=checkbox]');
-  let bodyChkboxes = table.querySelectorAll('tbody input[type=checkbox]');
+const ToggleAll = (TABLE) => {
+  let headChkbox = TABLE.querySelector('thead input[type=checkbox]');
+  let bodyChkboxes = TABLE.querySelectorAll('tbody input[type=checkbox]');
 
   headChkbox.addEventListener('change', () => {
     [...bodyChkboxes].map((i) => (i.checked = headChkbox.checked));
@@ -21,11 +21,11 @@ const DeleteRow = (e) => {
   row.parentNode.removeChild(row);
 };
 
-const DeleteAllRow = (table) => {
-  let delAllBtn = table.querySelector('thead .btn-del-all');
+const DeleteAllRow = (TABLE) => {
+  let delAllBtn = TABLE.querySelector('thead .btn-del-all');
 
   delAllBtn.addEventListener('click', () => {
-    let Chkboxes = table.querySelectorAll('tbody input[type=checkbox]');
+    let Chkboxes = TABLE.querySelectorAll('tbody input[type=checkbox]');
     [...Chkboxes].map((i) => {
       if (i.checked) i.closest('tr').remove();
     });
@@ -39,23 +39,22 @@ const DeleteAllRow = (table) => {
 });
 
 //file read
-const ReadFile = (CALLBACK, ENCODING) => {
+const ReadFile = (TABLE, ENCODING) => {
   const READER = new FileReader();
   const FILENAME = event.target.files[0].name;
 
   READER.readAsText(event.target.files[0], ENCODING); //파일읽기
   READER.onload = function () {
     //읽기완료
-    CALLBACK(READER, FILENAME);
+    AddTable(READER, FILENAME, TABLE);
   };
   event.target.value = '';
 };
 
 //file set
-const SetSqlBefore = (CONTENT, FILENAME) => {
+const AddTable = (CONTENT, FILENAME, TABLE) => {
   let newRow = document.createElement('tr');
-  let tableBody = tableSql.querySelector('tbody');
-
+  let tableBody = TABLE.querySelector('tbody');
   newRow.innerHTML =
     `
     <td><input type="checkbox" /></td>
@@ -71,26 +70,6 @@ const SetSqlBefore = (CONTENT, FILENAME) => {
 
   tableBody.appendChild(newRow);
 };
-const SetTableBefore = (CONTENT, FILENAME) => {
-  let newRow = document.createElement('tr');
-  let tableBody = tableCol.querySelector('tbody');
-
-  newRow.innerHTML =
-    `
-    <td><input type="checkbox" /></td>
-    <td>` +
-    FILENAME +
-    `</td>
-    <td>
-      <button class="btn-del" onclick="DeleteRow(this)">
-        <span class="lets-icons--dell-duotone"></span>
-      </button>
-    </td>
-  `;
-
-  tableBody.appendChild(newRow);
-};
-
 // add event listener
-fileLoaderSql.addEventListener('change', () => ReadFile(SetSqlBefore, 'UTF-8'));
-fileLoaderTable.addEventListener('change', () => ReadFile(SetTableBefore, 'UTF-8'));
+fileLoaderSql.addEventListener('change', () => ReadFile(tableSql, 'UTF-8'));
+fileLoaderTable.addEventListener('change', () => ReadFile(tableCol, 'UTF-8'));
